2c8b371ac826e0499427ed703e1682a580a9284f
unknown <ignacio.mele@hotmail.com>
2023-01-23 17:18:22 +0100

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,32 @@ import Skills from './components/skills/Skills';
 import Certifications from './components/certifications/Certifications';
 import Projects from './components/projects/Projects';
 import Contact from './components/contact/Contact';
+import Loader from './components/loader/Loader';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [showLoader, setShowLoader] = useState(false)
+  useEffect(() => {
+    setShowLoader(true)
+    setTimeout(() => {
+      setShowLoader(false)
+    }, 1000);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar/>
-        <Home/>
-        <About/>
-        <Skills/>
-        <Certifications/>
-        <Projects/>
-        <Contact/>
+        {showLoader ? <Loader/> : 
+        <div>
+          <Navbar/>
+          <Home/>
+          <About/>
+          <Skills/>
+          <Certifications/>
+          <Projects/>
+          <Contact/>
+          </div>}
+        
       </header>
     </div>
   );
--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -4,11 +4,14 @@ import './loader.css'
 export default function Loader() {
     return (
         <>
+        <div className="div-loader">
             <div class="three-body">
                 <div class="three-body__dot"></div>
                 <div class="three-body__dot"></div>
                 <div class="three-body__dot"></div>
             </div>
+
+        </div>
         </>
     )
 }
